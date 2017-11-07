3c51838b123390f3b6a1b8e19ebb68eb88263e3b
Pierre Boutillier <pierre_boutillier@hms.harvard.edu>
2017-11-07 10:49:26 -0500

    Dead rules in KappaStd

--- python/kappy/kappa_std.py
+++ python/kappy/kappa_std.py
@@ -7,6 +7,7 @@ __all__ = ['KappaStd']
 import subprocess
 import threading
 import json
+from os.path import join
 import abc
 from pkg_resources import resource_filename
 
@@ -24,9 +25,11 @@ class KappaStd(object):
     """
     def __init__(self, path=None, delimiter='\x1e', args=None):
         self.delimiter = delimiter
+        self.project_ast = None
+        self.analyses_to_init = True
         if not path:
-            path = resource_filename(__name__,"bin/KaSimAgent")
-        sim_args = [path,
+            path = resource_filename(__name__,"bin")
+        sim_args = [join(path,"KaSimAgent"),
                     "--delimiter",
                     "\\x{:02x}".format(ord(self.delimiter)),
                     "--log",
@@ -35,10 +38,19 @@ class KappaStd(object):
             sim_args = sim_args + args
         self.lock = threading.Lock()
         self.message_id = 0
-        self.popen = subprocess.Popen(sim_args,
-                                      stdin=subprocess.PIPE,
-                                      stdout=subprocess.PIPE,
-                                      stderr=subprocess.STDOUT)
+        self.sim_agent = subprocess.Popen(sim_args,
+                                          stdin=subprocess.PIPE,
+                                          stdout=subprocess.PIPE,
+                                          stderr=subprocess.STDOUT)
+        sa_args = [join(path,"KaSaAgent"),
+                    "--delimiter",
+                    "\\x{:02x}".format(ord(self.delimiter)), ]
+        if args:
+            sa_args = sa_args + args
+        self.sa_agent = subprocess.Popen(sa_args,
+                                         stdin=subprocess.PIPE,
+                                         stdout=subprocess.PIPE,
+                                         stderr=subprocess.STDOUT)
         return
 
     def __del__(self):
@@ -59,13 +71,13 @@ class KappaStd(object):
             message_id = self._get_message_id()
             message = {'id': message_id,'data': data}
             message = "{0}{1}".format(json.dumps(message), self.delimiter)
-            self.popen.stdin.write(message.encode('utf-8'))
-            self.popen.stdin.flush()
+            self.sim_agent.stdin.write(message.encode('utf-8'))
+            self.sim_agent.stdin.flush()
             buffer = bytearray()
-            c = self.popen.stdout.read(1)
+            c = self.sim_agent.stdout.read(1)
             while c != self.delimiter.encode('utf-8') and c:
                 buffer.extend(c)
-                c = self.popen.stdout.read(1)
+                c = self.sim_agent.stdout.read(1)
             response = json.loads(buffer.decode('utf-8'))
             if response["id"] != message_id:
                 raise KappaError(
@@ -78,15 +90,46 @@ class KappaStd(object):
         finally:
             self.lock.release()
 
+    def _dispatch_sa(self, method, args=None):
+        if args is not None:
+            data = [method, args]
+        else:
+            data = method
+
+        try:
+            self.lock.acquire()
+            message_id = self._get_message_id()
+            message = {'id': message_id,'data': data}
+            message = "{0}{1}".format(json.dumps(message), self.delimiter)
+            self.sa_agent.stdin.write(message.encode('utf-8'))
+            self.sa_agent.stdin.flush()
+            buffer = bytearray()
+            c = self.sa_agent.stdout.read(1)
+            while c != self.delimiter.encode('utf-8') and c:
+                buffer.extend(c)
+                c = self.sa_agent.stdout.read(1)
+            response = json.loads(buffer.decode('utf-8'))
+            if response['code'] == "SUCCESS":
+                return response['data']
+            else:
+                raise KappaError(response['data'])
+
+        finally:
+            self.lock.release()
+
     @abc.abstractmethod
     def projection(self, response):
         pass
 
     def shutdown(self):
-        if hasattr(self, 'popen'):
-            self.popen.stdin.close()
-            self.popen.stdout.close()
-            self.popen.kill()
+        if hasattr(self, 'sim_agent'):
+            self.sim_agent.stdin.close()
+            self.sim_agent.stdout.close()
+            self.sim_agent.kill()
+        if hasattr(self, 'sa_agent'):
+            self.sa_agent.stdin.close()
+            self.sa_agent.stdout.close()
+            self.sa_agent.kill()
 
     def projection(self, response):
         result_data = response["data"]["result_data"]
@@ -105,7 +148,9 @@ class KappaStd(object):
         """
         if overwrites is None:
             overwrites = []
-        return self._dispatch("ProjectParse", overwrites)
+        reply = self._dispatch("ProjectParse", overwrites)
+        self.project_ast = json.loads(reply['boxed_ast'])
+        return reply
 
     def file_create(self,file_object):
         """
@@ -114,12 +159,16 @@ class KappaStd(object):
         file_object -- a Kappa_common.File
         """
         file_data = file_object.toJSON()
+        self.project_ast = None
+        self.analyses_to_init = True
         return self._dispatch("FileCreate", file_data)
 
     def file_delete(self,file_id):
         """
         Remove a file from the project
         """
+        self.project_ast = None
+        self.analyses_to_init = True
         return self._dispatch("FileDelete", file_id)
 
     def file_get(self,file_id):
@@ -232,6 +281,8 @@ class KappaStd(object):
         ------
         simulation_parameter -- is described in kappa_common.SimulationParameter
         """
+        if self.project_ast is None:
+            raise KappaError("Project not parsed since last modification")
         return self._dispatch("SimulationStart",
                               simulation_parameter.toJSON())
 
@@ -240,3 +291,15 @@ class KappaStd(object):
         Restarts a paused simulation
         """
         return self._dispatch("SimulationContinue",pause_condition)
+
+    def _analyses_init(self):
+        if self.project_ast is None:
+            raise KappaError("Project not parsed since last modification")
+        result = self._dispatch_sa("INIT",self.project_ast)
+        self.analyses_to_init = False
+        return result
+
+    def analyses_dead_rules(self):
+        if self.analyses_to_init:
+            self._analyses_init()
+        return self._dispatch_sa("DEAD_RULES")
